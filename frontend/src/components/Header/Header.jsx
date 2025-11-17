7a8b511255ea7de54820750663954cc824b6d15a
NimWick638 <wnimantha638@gmail.com>
2025-11-17 23:00:44 +0530

    resolve error in app.jsx

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className = 'header'>
+        <div className='header-content'>
+            <h2>Order your foods here</h2>
+            <p>At DishDash, we believe delivery should be more than just fast—it should be seamless, reliable, and stress-free. Whether it’s a last-minute order, your weekly essentials, or a special surprise for someone you love, we make sure it reaches you on time and in perfect condition. With cutting-edge tracking, a trusted fleet, and customer-first service, we’re redefining the way deliveries feel: easy, secure, and always at your doorstep when you need them most.</p>   
+            <button>View More</button> 
+        </div>  
+    </div>
+  )
+}
+
+export default Header
