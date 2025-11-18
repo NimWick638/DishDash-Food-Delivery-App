a50dad157dfafb309713db46ca756b5e3b100ea4
NimWick638 <wnimantha638@gmail.com>
2025-11-18 15:02:47 +0530

    add styling for the header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 const App = () =>{
     return(<div className='app'>
       <NavBar/>
+      
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/Cart" element={<Cart/>} />
--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className = 'header'>
         <div className='header-content'>
-            <h2>Order your foods here</h2>
+            <h1>Order your foods here</h1>
             <p>At DishDash, we believe delivery should be more than just fast—it should be seamless, reliable, and stress-free. Whether it’s a last-minute order, your weekly essentials, or a special surprise for someone you love, we make sure it reaches you on time and in perfect condition. With cutting-edge tracking, a trusted fleet, and customer-first service, we’re redefining the way deliveries feel: easy, secure, and always at your doorstep when you need them most.</p>   
             <button>View More</button> 
         </div>  
