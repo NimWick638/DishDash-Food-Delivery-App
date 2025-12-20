77f5d63bd0206d4151e9b5e8ee4b9daa042fb9aa
NimWick638 <wnimantha638@gmail.com>
2025-12-20 16:58:33 +0530

    Add cart item count tracking with Context API and local state
    - Implement cartItems state in StoreContext
    - Add addToCart and removeFromCart functions
    - Connect FoodItem component to context for item counting

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,20 +1,34 @@
-import React from 'react'
+import React,{useContext,useState} from 'react'
 import './FoodItem.css'
 import { assets } from '../../assets/assets'
+import { StoreContext } from '../../context/StoreContext'
 
 const FoodItem = ({ id, name, price, description, image }) => {
+
+    
+    const {cartItems,addToCart,removeFromCart} = useContext(StoreContext);
+
+
     return (
         <div className="food-item">
             <div className='food-item-img-container'>
                 <img className='food-item-image' src={image} alt="" />
+                {
+                    !cartItems ? <img className='add' onClick={() => addToCart(id)} src={assets.add_icon_white} />
+                        : <div className='food-item-counter' >
+                            <img src={assets.remove_icon_red} onClick = {()=> removeFromCart(id) }alt="" className="reducer" />
+                            <p>{Math.max(cartItems[id],0)}</p>
+                            <img src={assets.add_icon_green} onClick = {()=> addToCart(id)} alt="" />
+                        </div>
+                }
             </div>
             <div className="food-item-info">
                 <div className="food-item-name-rating">
                     <p>{name}</p>
-                    <img src={assets.rating_starts}/>
+                    <img src={assets.rating_starts} />
                 </div>
                 <p className="food-item-desc">{description}</p>
-                <p className = "food-item-price">${price}</p>
+                <p className="food-item-price">${price}</p>
 
             </div>
         </div>
